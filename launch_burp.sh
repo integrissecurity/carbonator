52ec12440d729883ed3b0fb0b255c20d6ee9618f
Integris Security <carbonator@integrissecurity.com>
2014-08-06 18:07:59 -0400

    updated email command

--- launch_burp.sh
+++ launch_burp.sh
@@ -18,7 +18,7 @@ then
 		EMAIL=$5
 		echo Launching Scan against $1://$2:$3$4 EMailing reports to $5
 		java -jar -Xmx1024m ../burp_suite/burpsuite_pro_v1.6.02.jar $SCHEME $FQDN $PORT $FOLDER
-		echo 'Your scan results are attached to this email. Please visit https://www.integrissecurity.com/index.php?resources=Carbonator for more information.' | mutt -s 'Integris Security Carbonator Results' bcornell@integrissecurity.com -a IntegrisSecurity_Carbonator_$1_$2_$3.html && rm IntegrisSecurity_Carbonator_$1_$2_$3.html
+		echo 'Your scan results are attached to this email. Please visit https://www.integrissecurity.com/index.php?resources=Carbonator for more information.' | mutt -s 'Integris Security Carbonator Results' $5 -a IntegrisSecurity_Carbonator_$1_$2_$3.html && rm IntegrisSecurity_Carbonator_$1_$2_$3.html
 	else
 		echo Launching Scan against $1://$2:$3$4
 		java -jar -Xmx1024m ../burp_suite/burpsuite_pro_v1.6.02.jar $SCHEME $FQDN $PORT $FOLDER
